9328a887bb912c23be99c08ec41441d75ac98a72
dungvo1983 <dungvo1983@gmail.com>
2025-07-28 10:54:57 +0700

    123 - static html

---
+++ wp-content/themes/dung-university2/page2.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ocean.jpg)"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Our History</h1>
+    <div class="page-banner__intro">
+      <p>Learn how the school of your dreams got started.</p>
+    </div>
+  </div>
+</div>
+
+<div class="page-section page-section--beige">
+  <div class="container container--narrow generic-content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+  </div>
+</div>
+
+<div class="page-section page-section--white">
+  <div class="container container--narrow">
+    <h2 class="headline headline--medium">Biology Professors:</h2>
+
+    <ul class="professor-cards">
+      <li class="professor-card__list-item">
+        <a href="#" class="professor-card">
+          <img class="professor-card__image" src="<?php echo get_template_directory_uri(); ?>/images/barksalot.jpg" />
+          <span class="professor-card__name">Dr. Barksalot</span>
+        </a>
+      </li>
+      <li class="professor-card__list-item">
+        <a href="#" class="professor-card">
+          <img class="professor-card__image" src="<?php echo get_template_directory_uri(); ?>/images/meowsalot.jpg" />
+          <span class="professor-card__name">Dr. Meowsalot</span>
+        </a>
+      </li>
+    </ul>
+    <hr class="section-break" />
+
+    <div class="row group generic-content">
+      <div class="one-third">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+      </div>
+
+      <div class="one-third">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+      </div>
+
+      <div class="one-third">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
